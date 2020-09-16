0b279dd00fac885a848ae4e639e4a5362781447b
Seun Odewale <seunodewale.zone5@gmail.com>
2020-09-16 08:31:42 +0100

    setup spring project

---
+++ recipesJournal/src/main/java/com/seunmeme/demo/RecipresJournalApplication.java
@@ -0,0 +1,13 @@
+package com.seunmeme.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipresJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipresJournalApplication.class, args);
+    }
+
+}
