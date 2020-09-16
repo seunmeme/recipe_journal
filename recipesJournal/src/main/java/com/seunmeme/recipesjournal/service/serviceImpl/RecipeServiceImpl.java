20a7c5e605a232ba71c6a0e73313ead3b49f9aab
Seun Odewale <seunodewale.zone5@gmail.com>
2020-09-16 19:46:54 +0100

    add view al recipes and single recipe

---
+++ recipesJournal/src/main/java/com/seunmeme/recipesjournal/service/serviceImpl/RecipeServiceImpl.java
@@ -0,0 +1,30 @@
+package com.seunmeme.recipesjournal.service.serviceImpl;
+
+import com.seunmeme.recipesjournal.model.Recipe;
+import com.seunmeme.recipesjournal.repository.RecipeRepository;
+import com.seunmeme.recipesjournal.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Override
+    public void addRecipe(Recipe recipe) {
+        recipeRepository.save(recipe);
+    }
+
+    @Override
+    public Iterable<Recipe> getRecipes() {
+        return recipeRepository.findAll();
+    }
+
+    @Override
+    public Recipe getPostById(Long id) {
+        return recipeRepository.findRecipeById(id);
+    }
+
+}
